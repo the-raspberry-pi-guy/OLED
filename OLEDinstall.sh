3a8dff9f5a0a3471d36d3108c3bba851e2dc101c
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2013-12-28 19:10:08 +0000

    Create OLEDinstall.sh

---
+++ OLEDinstall.sh
@@ -0,0 +1,23 @@
+# This is a shell script to automate the process of install py-gaugette. How to download and use this is all covered in 
+# my OLED tutorial that can be found here:
+# This was worked out by The Raspberry Pi Guy! www.youtube.com/theraspberrypiguy
+
+sudo apt-get install git-core -y
+sudo apt-get install python-setuptools -y
+sudo apt-get install python-dev -y
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+sudo ./build
+git clone https://github.com/Gadgetoid/WiringPi2-Python.git
+cd WiringPi2-Python/
+sudo python setup.py install
+./build.sh
+cd ..
+git clone https://github.com/doceme/py-spidev.git
+cd py-spidev
+sudo python setup.py install
+cd ..
+git clone https://github.com/guyc/py-gaugette.git
+cd py-gaugette
+sudo python setup.py install
+cd
