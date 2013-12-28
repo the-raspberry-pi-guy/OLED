437cb14600f86d7beefb0142842a4415206cbf5c
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2013-12-28 19:18:29 +0000

    Create clock.py

---
+++ python-examples/clock.py
@@ -0,0 +1,40 @@
+# clock.py
+
+# This program interfaces with one of Adafruit's OLED displays and a Raspberry Pi (over SPI). It displays the current 
+# date (Day, Month, Year) and then scrolls to the current time. The program waits for 2 seconds between scroll.
+
+# Example code from the py-gaugette library... Simplified by The Raspberry Pi Guy
+
+import gaugette.ssd1306
+import time
+import sys
+
+RESET_PIN = 15
+DC_PIN    = 16
+
+led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
+led.begin()
+led.clear_display()
+
+offset = 0 # flips between 0 and 32 for double buffering
+
+while True:
+
+    # write the current time to the display on every other cycle
+    if offset == 0:
+        text = time.strftime("%A")
+        led.draw_text2(0,0,text,2)
+        text = time.strftime("%e %b %Y")
+        led.draw_text2(0,16,text,2)
+        text = time.strftime("%X")
+        led.draw_text2(0,32+4,text,3)
+        led.display()
+        time.sleep(5)
+    else:
+        time.sleep(5)
+
+    # vertically scroll to switch between buffers
+    for i in range(0,32):
+        offset = (offset + 1) % 64
+        led.command(led.SET_START_LINE | offset)
+        time.sleep(0.01)
