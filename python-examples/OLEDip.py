4e4d6f8f326416aff2998d3bf9d44b6fd666fac2
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-01-03 13:25:45 +0000

    Update OLEDip.py

--- python-examples/OLEDip.py
+++ python-examples/OLEDip.py
@@ -2,14 +2,15 @@
 
 # Meant for use with the Raspberry Pi and an Adafruit monochrome OLED display!
 
-# This program interfaces with the OLED display in order to print your current IP address to it! The program trys
+# This program interfaces with the OLED display in order to print your current IP address to it. The program trys
 # several methods in order to accquire an IP address. For example if you are using a WiFi dongle your IP will be 
 # different to when you are using a Ethernet cable. This program tests for both and if it can not detect one prints:
 # 'NO INTERNET!' to the display. This code is perfect to run on boot when you want to find your Pi's IP address for
-# SSH or VNC!
+# SSH or VNC.
 
 # This was coded by The Raspberry Pi Guy!
 
+# Imports all of the necessary modules
 import gaugette.ssd1306
 import time
 import sys
@@ -18,6 +19,7 @@ import fcntl
 import struct
 from time import sleep
 
+# This function allows us to grab any of our IP addresses
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
@@ -26,17 +28,16 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-
+# Sets our variables to be used later
 RESET_PIN = 15
 DC_PIN    = 16
 TEXT = ''
-count = 0
 
 led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
 led.clear_display()
 
-
+# This sets TEXT equal to whatever your IP address is, or isn't
 try:
     TEXT = get_ip_address('wlan0') # WiFi address of WiFi adapter. NOT ETHERNET
 except IOError:
@@ -45,7 +46,7 @@ except IOError:
     except IOError:
         TEXT = ('NO INTERNET!')
 
-
+# The actual printing of TEXT
 led.clear_display()
 intro = 'Hello!'
 ip = 'Your IP Address is:'
