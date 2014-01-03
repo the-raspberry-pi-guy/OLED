d9cc71c9570c0395af5a0bf8723cb3b64a97a8ef
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-01-03 11:40:10 +0000

    Update OLEDtext.py

--- python-examples/OLEDtext.py
+++ python-examples/OLEDtext.py
@@ -8,24 +8,28 @@
 
 # This program was created by The Raspberry Pi Guy!
 
+# Imports the necessary libraries... Gaugette 'talks' to the display ;-)
 import gaugette.ssd1306
 import time
 import sys
 
+# Setting some variables for our reset pin etc.
 RESET_PIN = 15
 DC_PIN    = 16
 
+# Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
 led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
 led.clear_display() # This clears the display but only when there is a led.display() as well!
 
-# led.draw_text2(x-axis, y-axis, whatyouwanttoprint, size)
+# led.draw_text2(x-axis, y-axis, whatyouwanttoprint, size) < Understand?
+
 # So led.drawtext2() prints simple text to the OLED display like so:
 
 text = 'Hello!'
 led.draw_text2(0,0,text,2)
 text2 = 'Hello!'
 led.draw_text2(0,16,text2,1)
-text = 'Hello!'
-led.draw_text2(32,25,text,1)
+text3 = 'Hello!'
+led.draw_text2(32,25,text3,1)
 led.display()
