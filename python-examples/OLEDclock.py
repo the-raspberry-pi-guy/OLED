54d1a494f7105bba715bbf96c818d87a65f76f23
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-01-03 14:05:38 +0000

    Update OLEDclock.py

--- python-examples/OLEDclock.py
+++ python-examples/OLEDclock.py
@@ -3,20 +3,25 @@
 # This program interfaces with one of Adafruit's OLED displays and a Raspberry Pi (over SPI). It displays the current 
 # date (Day, Month, Year) and then scrolls to the current time. The program waits for 2 seconds between scrolls.
 
-# Example code from the py-gaugette library... Simplified by The Raspberry Pi Guy
+# Example code from the py-gaugette library... Commented by The Raspberry Pi Guy
 
+# Imports the necessary modules
 import gaugette.ssd1306
 import time
 import sys
 
+# Sets up our pins again
 RESET_PIN = 15
 DC_PIN    = 16
+
 
 led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
 led.clear_display()
 
 offset = 0 # flips between 0 and 32 for double buffering
+
+# While loop has bulk of the code in it!
 
 while True:
 
